a43542058ccafe5c1dab0576decf4f5cb496255b
Léo Jacquet <leojacquet2003@gmail.com>
2023-05-15 20:55:04 +0200

    🔧 update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import sass from 'sass';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/todo-app',
+    css: {
+        preprocessorOptions: {
+            scss: {
+                implementation: sass
+            }
+        }
+    }
+});
