6fd8d8328e82af430af64601a2fdbfe90edaf5bf
Léo Jacquet <leojacquet2003@gmail.com>
2023-05-22 20:27:08 +0200

    ✨ add filters

---
+++ src/components/FilterBtn/FilterBtn.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { TodoFilter } from '../Main/Main';
+import './FilterBtn.scss';
+
+interface FilterBtnProps {
+    name: TodoFilter;
+    setFilter: (newFilter: TodoFilter) => void;
+    active: boolean;
+}
+
+const FilterBtn: FC<FilterBtnProps> = ({ name, setFilter, active }) => {
+    const handleClick = () => {
+        setFilter(name);
+    };
+
+    return (
+        <button className={'filterBtn' + (active ? ' active' : '')} onClick={handleClick}>
+            {name[0].toUpperCase() + name.slice(1)}
+        </button>
+    );
+};
+
+export default FilterBtn;
---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import FilterBtn from '../FilterBtn/FilterBtn';
+import { TodoFilter } from '../Main/Main';
+import './Filters.scss';
+
+interface FiltersProps {
+    currentFilter: TodoFilter;
+    setFilter: (newFilter: TodoFilter) => void;
+}
+
+const filters: TodoFilter[] = ['all', 'active', 'completed'];
+
+const Filters: FC<FiltersProps> = ({ currentFilter, setFilter }) => {
+    return (
+        <div className="filters">
+            {filters.map((filter) => (
+                <FilterBtn
+                    key={filter}
+                    name={filter}
+                    setFilter={setFilter}
+                    active={filter === currentFilter}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Filters;
